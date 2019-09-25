690818df5f1b5a43aec6cbad74c9e63b07b25083
codenm007 <n.majumder14@gmail.com>
2019-09-25 10:51:08 +0530

    Adding makrssub

--- makrsodsub_array.c
+++ makrsodsub_array.c
@@ -2,20 +2,26 @@
 main()
 {
 	int arr[5];
-	int i,n=5;
+	int i,sum=0;
 	//printf("Enter the no of elements:");
 	//scanf("%d",&n);
-	for(i=1;i<=n;i++)
+	for(i=1;i<=5;i++)
 	{
 		printf("\nEnter the marks of sub %d:",i);
 		scanf("%d",&arr[i]);
-		//printf("\nArray value : %d",arr[i]);
+		printf("\nArray value : %d",arr[i]);
+		sum = sum +arr[i];
+		
 	}
+	printf("\n\nsum of the array = %d",sum);
+	
     //for(i=0;i<n;i++)
-    i=1;
-    while(i<=n)
-    {   
-    	printf("\n Sub %d marks :  %d\n",i,arr[i]);
+   /*/ i=1;
+    int sum =0;
+    while(i<=5)
+    {  
+    	sum = sum +arr[i];
     	i++;
-	}
+	}	printf("\nsum of the array = %d",sum);
+	*/
 }
